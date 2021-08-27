6f5e6c091589b5e19da61d301f4048a71c53127a
Jovana Jovanovic <jjovana314@gmail.com>
2021-08-27 16:34:03 +0200

    UserModule imported

--- src/login/login.module.ts
+++ src/login/login.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { UserModule } from 'src/user/user.module';
 import { LoginController } from './login.controller';
 import { LoginService } from './login.service';
 
 @Module({
-  imports: [],
+  imports: [UserModule],
   controllers: [LoginController],
   providers: [LoginService],
 })
