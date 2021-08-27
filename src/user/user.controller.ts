a5e925134b86be515a5897a13199410e3922c3c4
Jovana Jovanovic <jjovana314@gmail.com>
2021-08-27 16:35:59 +0200

    addNewUser method created

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { UserService } from './user.service';
+import { UserDto } from './dto/user.dto';
+import { User } from './models/user.interface';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(
+    private readonly userService: UserService
+  ) { }
+
+  @Post()
+  async addNewUser(@Body() userData: UserDto): Promise<User> {
+    return await this.userService.addNewUser(userData);
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { UserDto } from './dto/user.dto';
+import { UserEntity } from './models/user.entity';
+import { User } from './models/user.interface';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>
+  ) { }
+
+  async addNewUser(userData: UserDto): Promise<User> {
+    return await this.userRepository.save(userData);
+  }
+}
