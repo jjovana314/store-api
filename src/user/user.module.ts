44fa095ac524e21ecb72886f331be3d8fbf5896a
Jovana Jovanovic <jjovana314@gmail.com>
2021-08-31 09:02:07 +0200

    imports updated

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,9 +3,19 @@ import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { UserEntity } from './models/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { NameEntity } from './models/name.entity';
+import { AddressEntity } from './models/address.entity';
+import { GeolocationEntity } from './models/geolocation.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity])],
+  imports: [
+    TypeOrmModule.forFeature([
+      UserEntity,
+      NameEntity,
+      AddressEntity,
+      GeolocationEntity
+    ])
+  ],
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService]
