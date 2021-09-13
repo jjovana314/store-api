1cfe9344e2d5469c77cebe8ea65fc7e66eb5ed74
Jovana Jovanovic <jjovana314@gmail.com>
2021-09-13 11:06:23 +0200

    Schemas imported

--- src/cart/cart.module.ts
+++ src/cart/cart.module.ts
@@ -3,10 +3,20 @@ import { CartController } from './cart.controller';
 import { CartService } from './cart.service';
 import { UsersModule } from 'src/users/users.module';
 import { ProductsModule } from 'src/products/products.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CartSchema } from './models/schemas/cart.schema';
+import { ProductsCartSchema } from './models/schemas/products.cart.schema';
 
 @Module({
-  imports: [UsersModule, ProductsModule],
-  controllers: [CartController],
-  providers: [CartService]
+    imports: [
+        UsersModule,
+        ProductsModule,
+        MongooseModule.forFeature([
+            { name: 'Cart', schema: CartSchema },
+            { name: 'ProductsCart', schema: ProductsCartSchema }
+        ])
+    ],
+    controllers: [CartController],
+    providers: [CartService]
 })
-export class CartModule {}
+export class CartModule { }
