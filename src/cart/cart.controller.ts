ba079040b7d425ca435b7f4387c98206baa13fca
Jovana Jovanovic <jjovana314@gmail.com>
2021-09-13 11:05:24 +0200

    addNewCart method added

--- src/cart/cart.controller.ts
+++ src/cart/cart.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CartService } from './cart.service';
+import { CartDto } from './models/dto/cart.dto';
+import { Cart } from './models/interfaces/cart.interface';
 
 @Controller('cart')
-export class CartController {}
+export class CartController {
+    constructor(
+        private readonly cartService: CartService
+    ) {}
+
+    @Post()
+    async addNewCart(@Body() cart: CartDto): Promise<Cart> {
+        return await this.cartService.addNewCart(cart);
+    }
+}
--- src/cart/cart.service.ts
+++ src/cart/cart.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CartDto } from './models/dto/cart.dto';
+import { Cart } from './models/interfaces/cart.interface';
 
 @Injectable()
-export class CartService {}
+export class CartService {
+    constructor(
+        @InjectModel('Cart') private readonly cartModel: Model<Cart>
+    ) {}
+
+    async addNewCart(cart: CartDto): Promise<Cart> {
+        const newCart = new this.cartModel(cart);
+        return await newCart.save();
+    }
+}
