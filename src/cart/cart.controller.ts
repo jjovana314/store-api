d122895265d5fed5a952eb89a65b562fec874568
Jovana Jovanovic <jjovana314@gmail.com>
2021-09-13 12:06:42 +0200

    logs added, getCarts and getCart methods created

--- src/cart/cart.controller.ts
+++ src/cart/cart.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CartService } from './cart.service';
 import { CartDto } from './models/dto/cart.dto';
 import { Cart } from './models/interfaces/cart.interface';
@@ -12,5 +12,15 @@ export class CartController {
     @Post()
     async addNewCart(@Body() cart: CartDto): Promise<Cart> {
         return await this.cartService.addNewCart(cart);
+    }
+
+    @Get()
+    async getCarts(): Promise<Cart[]> {
+        return await this.cartService.getCarts();
+    }
+
+    @Get(':id')
+    async getCart(@Param('id') id: string): Promise<Cart> {
+        return await this.cartService.getCart(id);
     }
 }
--- src/cart/cart.service.ts
+++ src/cart/cart.service.ts
@@ -1,17 +1,33 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { LogsService } from 'src/logs/logs.service';
 import { CartDto } from './models/dto/cart.dto';
 import { Cart } from './models/interfaces/cart.interface';
 
 @Injectable()
 export class CartService {
     constructor(
+        private readonly logsService: LogsService,
         @InjectModel('Cart') private readonly cartModel: Model<Cart>
     ) {}
 
     async addNewCart(cart: CartDto): Promise<Cart> {
         const newCart = new this.cartModel(cart);
-        return await newCart.save();
+        const result = await newCart.save();
+        this.logsService.addLogs(
+            'cart',
+            'created',
+            result._id
+        );
+        return await result;
+    }
+
+    async getCarts(): Promise<Cart[]> {
+        return await this.cartModel.find();
+    }
+
+    async getCart(id: string): Promise<Cart> {
+        return await this.cartModel.findById(id);
     }
 }
