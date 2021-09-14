1f115a23ebd019cfceee4e8d9169a036133ec019
Jovana Jovanovic <jjovana314@gmail.com>
2021-09-14 11:42:25 +0200

    class renamed

--- src/cart/models/dto/update.cart.dto.ts
+++ src/cart/models/dto/update.cart.dto.ts
@@ -1,4 +1,4 @@
 import { PartialType } from "@nestjs/mapped-types";
 import { CartDto } from "./cart.dto";
 
-export class UpdateCartsDto extends PartialType(CartDto) { }
+export class UpdateCartDto extends PartialType(CartDto) { }
