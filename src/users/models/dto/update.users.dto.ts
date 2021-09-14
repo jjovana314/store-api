3710c7d0c15cb59b76c1192af5b1b2c4084c5ecd
Jovana Jovanovic <jjovana314@gmail.com>
2021-09-14 11:42:52 +0200

    imports removed

--- src/users/models/dto/update.users.dto.ts
+++ src/users/models/dto/update.users.dto.ts
@@ -1,7 +1,3 @@
-import { UpdateAddressDto } from "./update.address.dto";
-import { UpdateNameDto } from "./update.name.dto";
-import { Type } from "class-transformer";
-import { ValidateNested, IsEmail } from "class-validator";
 import { UsersDto } from "./users.dto";
 import { PartialType } from "@nestjs/mapped-types";
 import { OmitType } from "@nestjs/mapped-types";
