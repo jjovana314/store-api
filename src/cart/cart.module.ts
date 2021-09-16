39f3fd4fe33620a01e17e6af4e91df1058fd5dde
Jovana Jovanovic <jjovana314@gmail.com>
2021-09-16 11:25:35 +0200

    ProductsModule and UsersModule imported

--- src/cart/cart.module.ts
+++ src/cart/cart.module.ts
@@ -5,10 +5,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { CartSchema } from './models/schemas/cart.schema';
 import { ProductsCartSchema } from './models/schemas/products.cart.schema';
 import { LogsModule } from 'src/logs/logs.module';
+import { ProductsModule } from 'src/products/products.module';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
     imports: [
         LogsModule,
+        ProductsModule,
+        UsersModule,
         MongooseModule.forFeature([
             { name: 'Cart', schema: CartSchema },
             { name: 'ProductsCart', schema: ProductsCartSchema }
